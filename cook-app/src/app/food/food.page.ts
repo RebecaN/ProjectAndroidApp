0bcbcfd564df516bdbd0d6f93970c05b1f313114
RebecaN <rebecanimat@yahoo.com>
2020-01-16 17:53:02 +0200

    experiment-url poze din db

--- cook-app/src/app/food/food.page.ts
+++ cook-app/src/app/food/food.page.ts
@@ -10,6 +10,8 @@ export class FoodPage implements OnInit {
 
   constructor(private apiService: ApiService) { }
 
+  ardei = "assets/ardei_iuti.jpg";
+  ardeiURL = `url(${this.ardei})`;
   ngOnInit() {
     this.apiService.getAllProducts().subscribe(res => {
       console.log(res);
