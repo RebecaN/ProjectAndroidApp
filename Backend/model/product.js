a42bf43af73c1f14edb08285b8d3bb0f49cb6ec4
Mihai-Alexandra-Gabriela <alexgabriela18@yahoo.com>
2020-01-25 14:39:46 +0200

    rezolvare eroare backend

--- Backend/model/product.js
+++ Backend/model/product.js
@@ -1,9 +1,21 @@
-class Product {
-    constructor(name, description, id) {
-      this.name = name;
-      this.description = description;
-      this.id = id;
-    }
+
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  id: {
+    type: String
   }
-  
-  module.exports = Product;
+});
+
+/**
+ * @typedef Iconset
+ */
+module.exports = mongoose.model("Product", productSchema);
