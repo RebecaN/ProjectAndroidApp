56730f5b434de108042da5abe49521864d63c15e
Mihai-Alexandra-Gabriela <alexgabriela18@yahoo.com>
2020-01-26 23:50:26 +0200

    selectare imagini

--- Backend/server.js
+++ Backend/server.js
@@ -9,14 +9,14 @@ console.log("---- Starting the backend -----")
  const Product = require("./model/Product");
 
  const mongoose = require("mongoose");
-//se creaza in mongo DB Legume
- const url = "mongodb://localhost:27017/Ingrediente";
- mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
- });
+//se creaza in mongo DB 
+  const url = "mongodb://localhost:27017/test";
+  mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+  });
 
-_fillUpDB();
+  _fillUpDB();
   app.use(cors());
   app.use("/", productRouter);
 
@@ -27,30 +27,9 @@ _fillUpDB();
 function _fillUpDB() {
   const products = [];
   products.push(
-    new Product({ name: "Ardei iute", description: "legume", id: 1 })
+    new Product({ name: "test", description: "test", id: 1 })
   );
-  products.push(
-    new Product({ name: "Ardei portocaliu", description: "legume", id: 2 })
-  );
-  products.push(
-    new Product({ name: "Ardei rosu", description: "legume", id: 3 })
-  );
-  products.push(
-    new Product({ name: "Ardei verde", description: "legume", id: 4 })
-  );
-  products.push(
-    new Product({ name: "Afine", description: "fructe", id: 5 })
-  );
-  products.push(
-    new Product({ name: "Ananas", description: "fructe", id: 6 })
-  );
-  products.push(
-    new Product({ name: "Avocado", description: "fructe", id: 7 })
-  );
-  products.push(
-    new Product({ name: "Banane", description: "fructe", id: 8 })
-  );
-
+  
   for (const p of products) {
     p.save();
   }
--- cook-app/src/app/desert/desert.page.ts
+++ cook-app/src/app/desert/desert.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from "src/app/api/api.service";
+import { Product } from "src/app/model/product.model";
 
 @Component({
   selector: 'app-desert',
@@ -7,9 +9,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DesertPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  public products: Array<Product> = [];
+  constructor(private apiService: ApiService) { }
+//URL Legume
+  ardei_I = "assets/legume/ardei_iuti.jpg";
+  ardeiIURL = `url(${this.ardei_I})`;
+  ardei_P = "assets/legume/ardei_porto.jpg";
+  ardeiPURL = `url(${this.ardei_P})`;
+  ardei_R = "assets/legume/ardei_rosii.jpg";
+  ardeiRURL = `url(${this.ardei_R})`;
+  ardei_V = "assets/legume/ardei_verzi.jpg";
+  ardeiVURL = `url(${this.ardei_V})`;
+  //URL Carbo
+  biscuiti = "assets/carbo/biscuiti.jpg";
+  biscuitiURL = `url(${this.biscuiti})`;
+  cuscus = "assets/carbo/cuscus.jpg";
+  cuscusURL = `url(${this.cuscus})`;
+  faina = "assets/carbo/faina.jpg";
+  fainaURL = `url(${this.faina})`;
+  gris = "assets/carbo/gris.jpg";
+  grisURL = `url(${this.gris})`;
+  //URL condimente
+  boia = "assets/condimente/boia.jpg";
+  boiaURL = `url(${this.boia})`;
+  bors = "assets/condimente/bors.jpg";
+  borsURL = `url(${this.bors})`;
+  busuioc = "assets/condimente/busuioc.jpg";
+  busuiocURL = `url(${this.busuioc})`;
+  cacao = "assets/condimente/cacao.jpg";
+  cacaoURL = `url(${this.cacao})`;
+   //URL fructe
+   afine = "assets/fructe/afine.png";
+   afineURL = `url(${this.afine})`;
+   ananas = "assets/fructe/ananas.jpg";
+   ananasURL = `url(${this.ananas})`;
+   avocado = "assets/fructe/avocado.jpg";
+   avocadoURL = `url(${this.avocado})`;
+   banane = "assets/fructe/banane.jpg";
+   bananeURL = `url(${this.banane})`;
+   //URL lactate
+   branza_brie = "assets/lactate/branza_brie.jpg";
+   branza_brieURL = `url(${this.branza_brie})`;
+   branza = "assets/lactate/branza.jpg";
+   branzaURL = `url(${this.branza})`;
+   cascaval = "assets/lactate/cascaval.jpg";
+   cascavalURL = `url(${this.cascaval})`;
+   kefir = "assets/lactate/kefir.jpg";
+   kefirURL = `url(${this.kefir})`;
+   //URL produse animale
+   bacon = "assets/prod_animal/bacon.jpg";
+   baconURL = `url(${this.bacon})`;
+   caras = "assets/prod_animal/caras.jpg";
+   carasURL = `url(${this.caras})`;
+   carnat = "assets/prod_animal/carnat.jpg";
+   carnatURL = `url(${this.carnat})`;
+   crap = "assets/prod_animal/crap.jpg";
+   crapURL = `url(${this.crap})`;
+   
+   ngOnInit() {
+    this.apiService.getAllProducts().subscribe(apiProducts => {
+      for (let prod of apiProducts) {
+        this.products.push(new Product(prod.name, prod.description, prod.id));
+      }
+    });
   }
 
 }
+
